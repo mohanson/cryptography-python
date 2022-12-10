bad7e55d2477e7d677feff48a18eb362cf02429d
mohanson <mohanson@outlook.com>
2022-12-10 13:33:52 +0800

    2022-12-10 13:33:52

--- bn128.py
+++ bn128.py
@@ -191,40 +191,16 @@ class Fp12(Fpx):
     p = [Fp(e) for e in [82, 0, 0, 0, 0, 0, -18, 0, 0, 0, 0, 0, 1]]  # w¹² - 18w⁶ + 82 = 0
 
 
-a = Fp2([Fp(3), Fp(0)])
-b = Fp2([Fp(9), Fp(1)])
-assert a + b == Fp2([Fp(12), Fp(1)])
-assert a - b == Fp2([Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd41),
-                     Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd46)])
-assert a * b == Fp2([Fp(27), Fp(3)])
-assert a / b == Fp2([Fp(0x2b149d40ceb8aaae81be18991be06ac3b5b4c5e559dbefa33267e6dc24a138e5),
-                     Fp(0x009713b03af0fed4cd2cafadeed8fdf4a74fa084e52d1852e4a2bd0685c315d2)])
-assert b ** 42 == Fp2([Fp(0x30644e72e131a029b85045b68181585aa4fe1cf011e7db175f2a2d79e17cfd47),
-                       Fp(0x30644e72e131a029b85045b6818158302983bf245a26e5c61975a0d46d9cfd47)])
+N = 21888242871839275222246405745257275088548364400416034343698204186575808495617
 
-a = Fp12([Fp(e) for e in [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]])
-b = Fp12([Fp(e) for e in [1, 2, 3, 4, 2, 2, 3, 4, 3, 2, 3, 4]])
-assert a * b == Fp12([Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87bcee8),
-                      Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87c404f),
-                      Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87c5483),
-                      Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87c6253),
-                      Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87c9029),
-                      Fp(0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87ceb79),
-                      Fp(0x0000000000000000000000000000000000000000000000000000000000003327),
-                      Fp(0x00000000000000000000000000000000000000000000000000000000000020b0),
-                      Fp(0x0000000000000000000000000000000000000000000000000000000000001d44),
-                      Fp(0x0000000000000000000000000000000000000000000000000000000000001a9a),
-                      Fp(0x0000000000000000000000000000000000000000000000000000000000001321),
-                      Fp(0x0000000000000000000000000000000000000000000000000000000000000438)])
-assert a / b == Fp12([Fp(0x2cd098d90a44e85af5a59c764154b991c4afaec7bcd14a447961b0bd1d7d1d81),
-                      Fp(0x2c9165696cd217b8d47c0e40cfe8f28a4c4cadccd8be630b036962ac44de2835),
-                      Fp(0x000818c49d384098612bfbea1300d2fd2bffb545c7801dfd83422cd0baff6b6b),
-                      Fp(0x16d6c80fddaa5d32be3c37ecb46ac06491cc2b10922e5b6bd929de6d21a15454),
-                      Fp(0x2a1ec289458c31cdd73c149d0ba84dc40fe7a19e56c4d8216dad2cb56e1b616b),
-                      Fp(0x0a1f284e8d2371a623abe8d6ac9a726239323f4fa9fce678670391061192afa7),
-                      Fp(0x03c6a4a7b63aaa1e2d796e72b84ae8ffac2f00f53bf70a281703f900291604d8),
-                      Fp(0x23d2fd15151a1b2d00962d62ef3ef7a6d2075a3cba98da69f808f90ebce3069d),
-                      Fp(0x05012062c1750dfea579794c4a28d4dca8234045fe11e51363be1fa69fe01372),
-                      Fp(0x0b3cab55aa0caf777904b997278f2034422b353d1be0795933a1d010c6b7adde),
-                      Fp(0x05f55060cf8c3b66984c4212c3c5b693d61e6dbf46c46d92cf428317d91c007b),
-                      Fp(0x2e0262f661d0c450b646bf2271134dbe8c306a6064e80aa15429af1a06d9a060)])
+# Curve order should be prime
+assert pow(2, N, N) == 2
+# Curve order should be a factor of P**12 - 1
+assert (P ** 12 - 1) % N == 0
+
+# Curve is y**2 = x**3 + 3
+B = Fp(3)
+# Twisted curve over FQ**2
+B2 = Fp2([Fp(3), Fp(0)]) / Fp2([Fp(9), Fp(1)])
+# Extension curve over FQ**12; same b value as over FQ
+B12 = Fp12([Fp(e) for e in [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
