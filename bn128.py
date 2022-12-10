c20cb7571714613ed4678a27e6cbf66ab2ecbc73
mohanson <mohanson@outlook.com>
2022-12-10 19:04:19 +0800

    2022-12-10 19:04:19

--- bn128.py
+++ bn128.py
@@ -204,3 +204,111 @@ B = Fp(3)
 B2 = Fp2([Fp(3), Fp(0)]) / Fp2([Fp(9), Fp(1)])
 # Extension curve over FQ**12; same b value as over FQ
 B12 = Fp12([Fp(e) for e in [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+
+class Fr(Fg):
+    p = N
+
+    def __repr__(self):
+        return f'Fr(0x{self.x:064x})'
+
+
+# Generator for curve over FQ
+G1 = (Fp(1), Fp(2))
+# Generator for twisted curve over FQ2
+G2 = (Fp2([Fp(0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed), Fp(0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2)]),
+      Fp2([Fp(0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa), Fp(0x090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b)]))
+
+
+def is_inf(pt):
+    # Check if a point is the point at infinity
+    return pt is None
+
+
+def is_on_curve(pt, b):
+    # Check that a point is on the curve defined by y**2 == x**3 + b
+    if is_inf(pt):
+        return True
+    x, y = pt
+    return y**2 - x**3 == b
+
+
+assert is_on_curve(G1, B)
+assert is_on_curve(G2, B2)
+
+# Elliptic curve doubling
+
+
+def double(pt):
+    x, y = pt
+    l = 3 * x**2 / (2 * y)
+    newx = l**2 - 2 * x
+    newy = -l * newx + l * x - y
+    return newx, newy
+
+# Elliptic curve addition
+
+
+def add(p1, p2):
+    if p1 is None or p2 is None:
+        return p1 if p2 is None else p2
+    x1, y1 = p1
+    x2, y2 = p2
+    if x2 == x1 and y2 == y1:
+        return double(p1)
+    elif x2 == x1:
+        return None
+    else:
+        l = (y2 - y1) / (x2 - x1)
+    newx = l**2 - x1 - x2
+    newy = -l * newx + l * x1 - y1
+    assert newy == (-l * newx + l * x2 - y2)
+    return (newx, newy)
+
+# Elliptic curve point multiplication
+
+
+def multiply(pt, n):
+    if n == 0:
+        return None
+    elif n == 1:
+        return pt
+    elif not n % 2:
+        return multiply(double(pt), n // 2)
+    else:
+        return add(multiply(double(pt), int(n // 2)), pt)
+
+
+def eq(p1, p2):
+    return p1 == p2
+
+
+# "Twist" a point in E(FQ2) into a point in E(FQ12)
+w = Fp12([Fp(e) for e in [0, 1] + [0] * 10])
+
+# Convert P => -P
+def neg(pt):
+    if pt is None:
+        return None
+    x, y = pt
+    return (x, -y)
+
+
+def twist(pt):
+    if pt is None:
+        return None
+    _x, _y = pt
+    # Field isomorphism from Z[p] / x**2 to Z[p] / x**2 - 18*x + 82
+    xcoeffs = [_x.coeffs[0] - _x.coeffs[1] * Fp(9), _x.coeffs[1]]
+    ycoeffs = [_y.coeffs[0] - _y.coeffs[1] * Fp(9), _y.coeffs[1]]
+    # Isomorphism into subfield of Z[p] / w**12 - 18 * w**6 + 82,
+    # where w**6 = x
+    nx = Fp12([xcoeffs[0], Fp(0), Fp(0), Fp(0), Fp(0), Fp(0), xcoeffs[1], Fp(0), Fp(0), Fp(0), Fp(0), Fp(0)])
+    ny = Fp12([ycoeffs[0], Fp(0), Fp(0), Fp(0), Fp(0), Fp(0), ycoeffs[1], Fp(0), Fp(0), Fp(0), Fp(0), Fp(0)])
+    # Divide x coord by w**2 and y coord by w**3
+    return (nx * w ** 2, ny * w**3)
+
+
+G12 = twist(G2)
+# Check that the twist creates a point that is on the curve
+assert is_on_curve(G12, B12)
