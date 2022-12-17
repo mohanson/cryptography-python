1c842ac28db3f22694c7587f0f6b3d7b3e4f338c
mohanson <mohanson@outlook.com>
2022-12-17 23:06:04 +0800

    2022-12-17 23:06:04

--- kzg.py
+++ kzg.py
@@ -2,7 +2,11 @@ import bn128
 import random
 import polynomial
 
-# Polynomial commits: https://cacr.uwaterloo.ca/techreports/2010/cacr2010-10.pdf
+# [1] Polynomial commits: https://cacr.uwaterloo.ca/techreports/2010/cacr2010-10.pdf
+# [2] https://www.youtube.com/watch?v=n4eiiCDhTes
+# [3] https://www.youtube.com/watch?v=NVvNHe_RGZ8
+# [4] https://copper-witch-857.notion.site/Polynomial-KZG-or-Kate-Commitment-DappLearning-Notes-fc426c8cb9a14878840852506865f13b
+
 
 Fq = bn128.Fq
 Fr = bn128.Fr
@@ -13,14 +17,15 @@ G2 = bn128.G2
 pairing = bn128.pairing
 
 sk = Fr(random.randint(0, Fr.p - 1))
-pk = [G1 * (sk**i) for i in range(10)]
+pk_g1 = [G1 * (sk**i) for i in range(10)]
+pk_g2 = [G2 * (sk**i) for i in range(10)]
 
 ax = [Fr(e) for e in [1,  2,  3,  4]]
 ay = [Fr(e) for e in [4, 15, 40, 85]]
 coeffs = polynomial.interp(ax, ay)
 commit = P1(Fq(0), Fq(0))
 for i in range(len(coeffs)):
-    commit = commit + pk[i] * coeffs[i]
+    commit = commit + pk_g1[i] * coeffs[i]
 
 px = Fr(1)
 py = Fr(4)
@@ -30,8 +35,8 @@ coeffs_2 = [-px, Fr(1)]
 coeffs_q = polynomial.div(coeffs_1, coeffs_2)
 proofs = I1
 for i in range(len(coeffs_q)):
-    proofs = proofs + pk[i] * coeffs_q[i]
+    proofs = proofs + pk_g1[i] * coeffs_q[i]
 
 lhs = pairing(G2, commit - G1 * py)
-rhs = pairing(G2 * sk - G2 * px, proofs)
+rhs = pairing(pk_g2[1] - G2 * px, proofs)
 assert lhs == rhs
