ef6e4037ae8d8a25c8c55702fbec7cd590f111d4
Mohanson <mohanson@outlook.com>
2022-12-19 21:36:21 +0800

    KZG batch proofs

--- kzg.py
+++ kzg.py
@@ -6,13 +6,14 @@ import polynomial
 # [2] https://www.youtube.com/watch?v=n4eiiCDhTes
 # [3] https://www.youtube.com/watch?v=NVvNHe_RGZ8
 # [4] https://copper-witch-857.notion.site/Polynomial-KZG-or-Kate-Commitment-DappLearning-Notes-fc426c8cb9a14878840852506865f13b
+# [5] https://foresightnews.pro/article/detail/17988
+# [6] https://dankradfeist.de/ethereum/2021/10/13/kate-polynomial-commitments-mandarin.html
 
 
-Fq = bn128.Fq
 Fr = bn128.Fr
-P1 = bn128.P1
 G1 = bn128.G1
 I1 = bn128.I1
+I2 = bn128.I2
 G2 = bn128.G2
 pairing = bn128.pairing
 
@@ -20,23 +21,29 @@ sk = Fr(random.randint(0, Fr.p - 1))
 pk_g1 = [G1 * (sk**i) for i in range(10)]
 pk_g2 = [G2 * (sk**i) for i in range(10)]
 
-ax = [Fr(e) for e in [1,  2,  3,  4]]
+ax = [Fr(e) for e in [0,  1,  2,  3]]
 ay = [Fr(e) for e in [4, 15, 40, 85]]
-coeffs = polynomial.interp(ax, ay)
-commit = I1
-for i in range(len(coeffs)):
-    commit = commit + pk_g1[i] * coeffs[i]
+fx = polynomial.interp(ax, ay)
+commit = sum([pk_g1[i] * fx[i] for i in range(len(fx))], start=I1)
 
+# 单个证明
 px = Fr(1)
-py = Fr(4)
-coeffs_1 = [e for e in coeffs]
-coeffs_1[0] = coeffs_1[0] - py
-coeffs_2 = [-px, Fr(1)]
-coeffs_q = polynomial.div(coeffs_1, coeffs_2)
-proofs = I1
-for i in range(len(coeffs_q)):
-    proofs = proofs + pk_g1[i] * coeffs_q[i]
-
+py = Fr(15)
+qx = polynomial.div(polynomial.sub(fx, [py]), [-px, Fr(1)])
+proofs = sum([pk_g1[i] * qx[i] for i in range(len(qx))], start=I1)
 lhs = pairing(G2, commit - G1 * py)
 rhs = pairing(pk_g2[1] - G2 * px, proofs)
+assert lhs == rhs
+
+# 批量证明
+px = [Fr(e) for e in [0,  1]]
+py = [Fr(e) for e in [4, 15]]
+ix = polynomial.interp(px, py)
+zx = polynomial.vanish(px)
+qx = polynomial.div(polynomial.sub(fx, ix), zx)
+proofs = sum([pk_g1[i] * qx[i] for i in range(len(qx))], start=I1)
+ix_sg1 = sum([pk_g1[i] * ix[i] for i in range(len(ix))], start=I1)
+zx_sg2 = sum([pk_g2[i] * zx[i] for i in range(len(zx))], start=I2)
+lhs = pairing(G2, commit - ix_sg1)
+rhs = pairing(zx_sg2, proofs)
 assert lhs == rhs
